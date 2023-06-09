fd0c0f30b0fd257b0ba2110ed2e2d8d41adea262
unknown <18mishak2000@gmail.com>
2023-06-09 20:56:23 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+let express = require('express');
+let app = express();
+/**
+ * public - имя папки где хранится статика
+ */
+app.use(express.static('public'));
+/**
+ *  задаем шаблонизатор
+ */
+app.set('view engine', 'pug');
+/**
+* Подключаем mysql модуль
+*/
+let mysql = require('mysql');
+/**
+* настраиваем модуль
+*/
+
+let con = mysql.createConnection({
+  host: 'localhost',
+  user: 'miller',
+  password: 'root',
+  database: 'lite_shop'
+});
+
+app.listen(3000, function () {
+  console.log('node express work on 3000');
+});
+
+app.get('/', function (req, res) {
+  con.query(
+    'SELECT * FROM goods',
+    function (error, result) {
+      if (error) throw error;
+      let goods = {};
+      for (let i = 0; i < result.length; i++) {
+        goods[result[i]['id']] = result[i];
+      } 
+      //console.log(goods);
+      // console.log(JSON.parse(JSON.stringify(goods)));
+      res.render('main', {
+        foo: 'hello',
+        bar: 7,
+        goods: JSON.parse(JSON.stringify(goods))
+      });
+    }
+  );
+});
+
+app.get('/cat', function (req, res) {
+  console.log(req.query.id);
+  let catId = req.query.id;
+
+  let cat = new Promise(function (resolve, reject) {
+    con.query(
+      'SELECT * FROM category WHERE id=' + catId,
+      function (error, result) {
+        if (error) reject(error);
+        resolve(result);
+      });
+  });
+  let goods = new Promise(function (resolve, reject) {
+    con.query(
+      'SELECT * FROM goods WHERE category=' + catId,
+      function (error, result) {
+        if (error) reject(error);
+        resolve(result);
+      });
+  });
+
+  Promise.all([cat, goods]).then(function (value) {
+    console.log(value[0]);
+    res.render('cat', {
+      cat: JSON.parse(JSON.stringify(value[0])),
+      goods: JSON.parse(JSON.stringify(value[1]))
+    });
+  })
+});
+
+app.get('/goods', function (req, res) {
+  console.log(req.query.id);
+  con.query('SELECT * FROM goods WHERE id=' + req.query.id, function (error, result, fields) {
+    if (error) throw error;
+    res.render('goods', { goods: JSON.parse(JSON.stringify(result)) });
+  });
+});
